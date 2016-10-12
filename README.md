0c77014ff110e19171f488c683ddf89f4f39d657
tgergely <tudor.gergely@catalysts.cc>
2016-10-12 14:21:39 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Catalysts React workshop
+========================
+
+
+Building our own football manager application.
+
+What you need to have installed:
+
+- [NodeJS](https://nodejs.org/en/)
+- [Git](https://git-scm.com/)
+- Your favorite text editor!
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Catalysts workshop football manager
+            </div>
+        );
+    }
+}
+
+export default App;
